23ca06693be3b4aab9a85f3497b72f0bc3a43cb3
12111323-rohit <122770978+12111323-rohit@users.noreply.github.com>
2025-05-25 17:28:34 +0000

    container config done

---
+++ .devcontainer/01_Basics/01_Vaiables.js
@@ -0,0 +1,17 @@
+const accountId =12111323
+let accountEmail ="rohityadaav125@gmail.com"
+var accountPassword ="Naya2025"
+accountCity ="Delhi"
+let accountState
+
+
+accountEmail ="rohit@gmail.com"
+accountPassword="123445"
+accountCity="Punjab"
+
+/*
+Prefer not to use  var 
+because of issue in block scope and functional scope
+*/
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
---
+++ .devcontainer/01_Basics/02_datatypes.js
@@ -0,0 +1,2 @@
+"use strict" ;//treat all JS codes as newer Version//
+// alert(3+3) we are using nodeJs not browser//

066d5e704ef91ddc4692fb1db63ebb6241cf8689
12111323-rohit <122770978+12111323-rohit@users.noreply.github.com>
2025-05-26 08:06:15 +0000

    Datatypes

--- .devcontainer/01_Basics/02_datatypes.js
+++ .devcontainer/01_Basics/02_datatypes.js
@@ -1,2 +1,14 @@
 "use strict" ;//treat all JS codes as newer Version//
-// alert(3+3) we are using nodeJs not browser//
+// alert(3+3) we are using nodeJs not browser
+
+
+// number => 2 to the power 53
+// bigint
+// string =>""
+// boolean=>true/false
+// undefined =>
+// symbol => unique
+
+console.log(typeof undefined) // undefind
+console.log(typeof null) //object
+
